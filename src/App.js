2b430e86080ad5e5004667d17a4fce70023e2ee9
Aritra1296 <aritrivu@gmail.com>
2021-08-09 13:30:23 +0530

    message pusher

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import React, {useState, useEffect } from 'react'
 import './App.css'
 import Chat from './chat/Chat'
 import Sidebar from './sidebar/Sidebar'
+import Pusher from 'pusher-js'
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher('28527e97292ee45133a6', {
+      cluster: 'ap2',
+    })
+    const channel = pusher.subscribe('messages')
+    channel.bind('inserted', function (data) {
+      alert(JSON.stringify(data))
+    })
+  }, [])
+
   return (
     <div className='App'>
-
       <div className='app_body'>
         <Sidebar />
         <Chat />
       </div>
-      
     </div>
   )
 }
